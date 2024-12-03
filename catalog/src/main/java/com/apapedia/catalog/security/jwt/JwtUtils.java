8a7ebe260b060779f667cc3c178d5dbb5b95420c
Devina <devina.hana@ui.ac.id>
2024-12-04 00:50:04 +0700

    feat: add authentication for catalog service

---
+++ catalog/src/main/java/com/apapedia/catalog/security/jwt/JwtUtils.java
@@ -0,0 +1,52 @@
+package com.apapedia.catalog.security.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import io.jsonwebtoken.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${catalog.app.jwtSecret}")
+    private String jwtSecret;
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String getClaimFromJwtToken(String token, String attribute){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().get(attribute).toString();
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e){
+            logger.error("Invalid JWT Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e){
+            logger.error("Invalid JWT Token: {}", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JWT token i unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+}
