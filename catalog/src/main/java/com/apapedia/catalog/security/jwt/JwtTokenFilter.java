7d44c5451bf9d00348a4bcd3c14f308603b9e67d
Devina <devina.hana@ui.ac.id>
2024-12-05 11:47:45 +0700

    fix: fix response for invalid jwt token

--- catalog/src/main/java/com/apapedia/catalog/security/jwt/JwtTokenFilter.java
+++ catalog/src/main/java/com/apapedia/catalog/security/jwt/JwtTokenFilter.java
@@ -13,8 +13,14 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.apapedia.catalog.dto.response.BaseResponse;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,28 +29,42 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtils jwtUtils;
 
+    private Gson gson = new Gson();
+    
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
             String jwt = parseJwt(request);
-            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt);
-                String userId = jwtUtils.getClaimFromJwtToken(jwt, "id");
-                request.setAttribute("userId", userId);
-                String role = jwtUtils.getClaimFromJwtToken(jwt, "role");
+            if (jwt == null || jwt.isEmpty()) {
+                throw new IllegalArgumentException("JWT String argument cannot be null or empty.");
+            } else if (!jwtUtils.validateJwtToken(jwt)) {
+                throw new IllegalArgumentException("JWT token is invalid");
+            } 
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
+            String userId = jwtUtils.getClaimFromJwtToken(jwt, "id");
+            request.setAttribute("userId", userId);
+            String role = jwtUtils.getClaimFromJwtToken(jwt, "role");
 
-                Set<GrantedAuthority> grantedAuthoritySet = new HashSet<>();
-                grantedAuthoritySet.add(new SimpleGrantedAuthority(role));
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        username, null, grantedAuthoritySet);
+            Set<GrantedAuthority> grantedAuthoritySet = new HashSet<>();
+            grantedAuthoritySet.add(new SimpleGrantedAuthority(role));
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    username, null, grantedAuthoritySet);
 
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         } catch (Exception e) {
             logger.error("Cannot set user authentication: " + e.getMessage());
+            String responseJsonString = this.gson
+                    .toJson(new BaseResponse<>(false, "Cannot set user authentication: " + e.getMessage()));
+            PrintWriter out = response.getWriter();
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            out.print(responseJsonString);
+            out.flush();
             return;
         }
         filterChain.doFilter(request, response);
